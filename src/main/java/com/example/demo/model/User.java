5790695651bb2cfd0a96931bf831c3c9a47489a2
Melissa Santos <melissasantos2202@gmail.com>
2024-07-26 16:47:20 -0600

    user class done

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,28 @@
 package com.example.demo.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User  implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    //password does not need to be unique
+    @Column(nullable = false)
+
+
 }
