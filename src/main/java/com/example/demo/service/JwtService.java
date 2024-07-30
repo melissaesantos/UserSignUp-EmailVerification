1cdcf86a7d2c9f03a984bb7a778e394b5d0b0c35
Melissa Santos <melissasantos2202@gmail.com>
2024-07-30 16:25:43 -0600

    fixed service and value error

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -1,17 +1,17 @@
 package com.example.demo.service;
 
-
-import io.jsonwebtoken.Claims;
-import  org.springframework.stereotype.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 //the spring service component allows it to be automatically discovered and instantiated
+
 @Service
 public class JwtService {
     //this is taking the value from our security.jw.secretkey in our application.properties
-    @value("${security.jwt.secret.key}")
+    @Value("${security.jwt.secret-key}")
     private String secretKey;
 
-    @value("${security.jwt.expiration-time}")
+    @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
     //this is extracting the username form our JWT token
     public String extractUsername(String token) {
