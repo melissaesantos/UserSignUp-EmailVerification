ab6bd649e123dd0bd174e88ad47094fb74c2fb38
Melissa Santos <melissasantos2202@gmail.com>
2024-07-30 16:06:20 -0600

    fixed Optional error

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.example.demo.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository  extends CrudRepository<User, Long> {
-    //did optional since it doesnt guarantee that we will find a user.
+    //did optional since it doesn't guarantee that we will find a user.
     //now we can find the user by their email
     Optional<User> findByEmail(String email);
     //we will now be doing the same thing but by verification code
