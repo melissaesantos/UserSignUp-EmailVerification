1e091d7d572589514c61b8e4159a8392ab4ff69b
Melissa Santos <melissasantos2202@gmail.com>
2024-07-30 16:02:50 -0600

    user lookup by email

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.repository;
 
-public interface UserRepository {
+import com.example.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends CrudRepository<User, Long> {
+    //did optional since it doesnt guarantee that we will find a user.
+    //now we can find the user by their email
+    Optional<User> findByEmail(String email);
+
 }
