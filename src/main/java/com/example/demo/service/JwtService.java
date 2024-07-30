6cf09fbb319c8d367ff04fc20ad4067d8e739f20
Melissa Santos <melissasantos2202@gmail.com>
2024-07-30 16:18:17 -0600

    checking secret key and expiration time

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -1,7 +1,14 @@
 package com.example.demo.service;
-
+import lombok.Value;
+import  org.springframework.stereotype.Value;
 import org.springframework.stereotype.Service;
 //the spring service component allows it to be automatically discovered and instantiated
 @Service
 public class JwtService {
+    //this is taking the value from our security.jw.secretkey in our application.properties
+    @value("${security.jwt.secret.key}")
+    private String secretKey;
+
+    @value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
 }
