acafec206df5c3391e9615e2c2844bbceed03c4a
Melissa Santos <melissasantos2202@gmail.com>
2024-07-30 16:13:56 -0600

    created jwt class and spring service

---
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+//the spring service component allows it to be automatically discovered and instantiated
+@Service
+public class JwtService {
+}
