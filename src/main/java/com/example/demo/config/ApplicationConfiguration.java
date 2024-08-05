cfe74a38fb3308e41f7a259ed5d65e3fc4da264c
Melissa Santos <melissasantos2202@gmail.com>
2024-08-05 16:16:43 -0600

    finished beans

--- src/main/java/com/example/demo/config/ApplicationConfiguration.java
+++ src/main/java/com/example/demo/config/ApplicationConfiguration.java
@@ -4,8 +4,13 @@ package com.example.demo.config;
 import com.example.demo.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -22,5 +27,28 @@ public class ApplicationConfiguration {
         //we are using a lambda expression to find an email by the repository
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        //this is encoding the passwords securely
+        return new BCryptPasswordEncoder();
+    }// so lets say the password is MElISSA in the database it will not show up like that because of the encryption
+
+    //this next bean is going to be our authentication manager
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+    //authentication provider bean
+
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return  authProvider;
     }
 }
