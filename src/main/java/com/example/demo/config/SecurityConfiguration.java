1499122ba9a33e87502087f172989853f2419b85
Melissa Santos <melissasantos2202@gmail.com>
2024-08-05 17:06:56 -0600

    Security COngif constructor done

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -1,4 +1,24 @@
 package com.example.demo.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+//this indicates that this class enables spring security's
+//web security support
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfiguration(
+            JWTAuthenticationFilter jwtAuthenticationFilter,
+            AuthenticationProvider authenticationProvider
+    ){
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 }
