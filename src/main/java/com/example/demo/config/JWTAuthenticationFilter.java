14db036355d2cccded878280c64cad3c9b3e89dc
Melissa Santos <melissasantos2202@gmail.com>
2024-08-05 16:24:46 -0600

    created constructor to inject the dependencies

--- src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 
 import com.example.demo.service.JwtService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -17,5 +18,17 @@ public class JWTAuthenticationFilter {
     private final JwtService jwtService;
 
     private final UserDetailsService userDetailsService;
+
+    //now we will define a constructor to inject these dependencies and initialize the instance variables
+
+    public JwtAuthenticationFilter(
+            JwtService jwtService,
+            UserDetailsService userDetailsService,
+            HandlerExceptionResolver handlerExceptionResolver
+    ){
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
+    }
 
 }
