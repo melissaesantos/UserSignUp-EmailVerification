0c5fd21450dd4ad427a54e92c14020916501c996
Melissa Santos <melissasantos2202@gmail.com>
2024-08-05 16:59:18 -0600

    JWT AUTHEN FILTER DONE

--- src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
@@ -2,28 +2,83 @@ package com.example.demo.config;
 
 
 import com.example.demo.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
 
 //**
 // @component is marking this class as a spring component which allows it to be automactically
 //detected and instantiated by springs component scanning
 // */
 @Component
-public class JWTAuthenticationFilter {
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
 
     private final JwtService jwtService;
 
     private final UserDetailsService userDetailsService;
+    private final AuthenticationProvider authenticationProvider;
 
     //now we will define a constructor to inject these dependencies and initialize the instance variables
-    public JWTAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver, JwtService jwtService, UserDetailsService userDetailsService) {
+    public JWTAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver, JwtService jwtService, UserDetailsService userDetailsService, AuthenticationProvider authenticationProvider) {
         this.handlerExceptionResolver = handlerExceptionResolver;
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+
+            )throws IOException, ServletException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try{
+            final String jwt = authHeader.substring(7);
+            final String userEmail = jwtService.extractUsername(jwt);
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(userEmail != null && authentication == null){
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+                if(jwtService.isTokenValid(jwt, userDetails)){
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+            filterChain.doFilter(request, response);
+        }catch (Exception e){
+            handlerExceptionResolver.resolveException(request, response, null, e);
+        }
     }
 
 
