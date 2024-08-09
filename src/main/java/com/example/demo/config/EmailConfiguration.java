ca5657a7ab6436db2a97d7e3e9747cced28eb4cd
Melissa Santos <melissasantos2202@gmail.com>
2024-08-08 19:11:53 -0600

    finished mail sender and now we are done with authentication

--- src/main/java/com/example/demo/config/EmailConfiguration.java
+++ src/main/java/com/example/demo/config/EmailConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Properties;
+
 @Configuration
 public class EmailConfiguration {
     @Value("${spring.mail.username}")
@@ -20,7 +22,19 @@ public class EmailConfiguration {
     public JavaMailSender javaMailSender() {
         //now we are creating the javaMailSender object
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailUsername);
+        mailSender.setPassword(password);
 
+        //now we are setting up the propeties
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
 
     }
 }
