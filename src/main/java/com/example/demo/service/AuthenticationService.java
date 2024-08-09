b126b7fb5cb523c1fe0664a4f6d505214b162d37
Melissa Santos <melissasantos2202@gmail.com>
2024-08-08 19:49:50 -0600

    user signup registration complete

--- src/main/java/com/example/demo/service/AuthenticationService.java
+++ src/main/java/com/example/demo/service/AuthenticationService.java
@@ -2,10 +2,12 @@ package com.example.demo.service;
 //this contains the logic for both authenticating existing users and creating
 //new users
 
+import com.example.demo.dto.LoginUserDto;
 import com.example.demo.dto.RegisteredUserDTO;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -35,7 +37,7 @@ public class AuthenticationService {
         //we use the password encoder that way the password stays hidden
         User user = new User(input.getUserName(), input.getEmail(), passwordEncoder.encode(input.getPassword()));
         user.setVerificationCode(generateVerificationCode());
-        user.setVerificationCodeExpiresAt(LocalDateTime.now().plus(15);
+        user.setVerificationCodeExpiresAt(LocalDateTime.now().plusMinutes(15));
         //we are gonna set them to not be enabled because they havent verified their email yet
         user.setEnabled(false);
         //now this is sending that verification email
@@ -43,6 +45,16 @@ public class AuthenticationService {
         //now this is saving the user
         return userRepository.save(user);
 
+    }
+
+    //now we are working on the actual authentication
+    public User authenticate(LoginUserDto input){
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        //so right now we are taking care of the case in which they login but they have not verified their account
+        if(!user.isEnabled()){
+            throw new RuntimeException("User is not enabled");
+        }
     }
 
 }
